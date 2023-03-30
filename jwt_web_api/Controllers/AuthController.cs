63ec5b5047b597ae78049ed22d8ed82fca7985b5
winsanity <stephencurry30@inbox.ru>
2023-03-31 02:08:44 +0300

    Add project files.

---
+++ jwt_web_api/Controllers/AuthController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace jwt_web_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        public static User user = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            CreatePassHash(request.Password, out byte[] passHash, out byte[] passSalt);
+
+            user.UserName = request.UserName;
+            user.PassHash = passHash;
+            user.PassSalt = passSalt;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+            if (user.UserName != request.UserName)
+                return BadRequest("user not found!");
+
+            if (!VerifyPassHash(request.Password, user.PassHash, user.PassSalt))
+                return BadRequest("wrong password");
+
+            string token = CreateToken(user);
+
+            return Ok(token);
+        }
+
+        private bool VerifyPassHash(string password, byte[] passHash, byte[] passSalt)
+        {
+            using(var hmac = new HMACSHA512(passSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passHash);
+            }
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
+        private void CreatePassHash(string password, out byte[] passHash, out byte[] passSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passSalt = hmac.Key;
+                passHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
---
+++ jwt_web_api/User.cs
@@ -0,0 +1,9 @@
+﻿namespace jwt_web_api
+{
+    public class User
+    {
+        public string UserName { get; set; } = string.Empty;
+        public byte[] PassHash { get; set; }
+        public byte[] PassSalt { get; set; }
+    }
+}
