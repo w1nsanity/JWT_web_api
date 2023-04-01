6cf6f29e08177b713397f758368b22470b2f8523
winsanity <stephencurry30@inbox.ru>
2023-04-01 18:24:49 +0300

    + refresh token!

--- jwt_web_api/User.cs
+++ jwt_web_api/User.cs
@@ -5,5 +5,8 @@
         public string UserName { get; set; } = string.Empty;
         public byte[] PassHash { get; set; }
         public byte[] PassSalt { get; set; }
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime TokenCreated { get; set; }
+        public DateTime TokenExpires { get; set; }
     }
 }
